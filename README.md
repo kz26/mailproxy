a58387677cc48ca924e0ba79eb65c4962636d4db
kz26 <kz@localhost>
2017-10-08 18:08:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# mailproxy
+mailproxy is a simple SMTP proxy. It receives emails through an unencrypted, unauthenticated SMTP interface and retransmits them through a remote SMTP server that requires modern features such as encryption (SSL, STARTTLS) and/or authentication (SMTP AUTH). mailproxy is primarily useful for enabling email functionality in legacy software that only supports plain SMTP.
+
+# Requirements
+* Python 3.5+
+* [aiosmtpd 1.1+](https://aiosmtpd.readthedocs.io)
+
+
+# Usage
+1. Create a config file (see below).
+2. Run mailproxy from the command line, e.g. `python mailproxy.py`
+By default, mailproxy looks for a `config.ini` in its own directory.
+If you have placed your config file elsewhere, you can run mailproxy
+using `python mailproxy.py <config_file_path>`.
+
+
+# Configuration
+An example config file appears below:
+```
+[local]
+host = 127.0.0.1
+port = 25
+
+[remote]
+host = smtp.gmail.com
+port = 465
+use_ssl = yes
+starttls = no
+smtp_auth = yes
+smtp_auth_user = USERNAME
+smtp_auth_password = PASSWORD
+```
---
+++ mailproxy.py
@@ -0,0 +1,103 @@
+import asyncio
+import configparser
+import logging
+import os
+import smtplib
+import sys
+
+from aiosmtpd.controller import Controller
+
+
+__version__ = '1.0.0'
+
+class MailProxyHandler:
+    def __init__(self, host, port=0, auth=None, use_ssl=False, starttls=False):
+        self._host = host
+        self._port = port
+        auth = auth or {}
+        self._auth_user = auth.get('user')
+        self._auth_password = auth.get('password')
+        self._use_ssl = use_ssl
+        self._starttls = starttls
+
+    async def handle_DATA(self, server, session, envelope):
+        try:
+            refused = self._deliver(envelope)
+        except smtplib.SMTPRecipientsRefused as e:
+            logging.info('Got SMTPRecipientsRefused: %s', refused)
+            return "553 Recipients refused {}".format(' '.join(refused.keys()))
+        except smtplib.SMTPResponseException as e:
+            return "{} {}".format(e.smtp_code, e.smtp_error)
+        else:
+            if refused:
+                logging.info('Recipients refused: %s', refused)
+            return '250 OK'
+
+    # adapted from https://github.com/aio-libs/aiosmtpd/blob/master/aiosmtpd/handlers.py
+    def _deliver(self, envelope):
+        refused = {}
+        try:
+            if self._use_ssl:
+                s = smtplib.SMTP_SSL()
+            else:
+                s = smtplib.SMTP()
+            s.connect(self._host, self._port)
+            if self._starttls:
+                s.starttls()
+                s.ehlo()
+            if self._auth_user and self._auth_password:
+                s.login(self._auth_user, self._auth_password)
+            try:
+                refused = s.sendmail(
+                    envelope.mail_from,
+                    envelope.rcpt_tos,
+                    envelope.original_content
+                )
+            finally:
+                s.quit()
+        except (OSError, smtplib.SMTPException) as e:
+            logging.exception('got %s', e.__class__)
+            # All recipients were refused. If the exception had an associated
+            # error code, use it.  Otherwise, fake it with a SMTP 554 status code. 
+            errcode = getattr(e, 'smtp_code', 554)
+            errmsg = getattr(e, 'smtp_error', e.__class__)
+            raise smtplib.SMTPResponseException(errcode, errmsg.decode())
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        config_path = sys.argv[1]
+    else:
+        config_path = os.path.join(
+            os.getcwd(),
+            'config.ini'
+        )
+    if not os.path.exists(config_path):
+        raise Exception("Config file not found: {}".format(config_path))
+
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    
+    use_auth = config.getboolean('remote', 'smtp_auth', fallback=False)
+    if use_auth:
+        auth = {
+            'user': config.get('remote', 'smtp_auth_user'),
+            'password': config.get('remote', 'smtp_auth_password')
+        }
+    else:
+        auth = None
+    
+    controller = Controller(
+        MailProxyHandler(
+            host=config.get('remote', 'host'),
+            port=config.getint('remote', 'port', fallback=25),
+            auth=auth,
+            use_ssl=config.getboolean('remote', 'use_ssl',fallback=False),
+            starttls=config.getboolean('remote', 'starttls',fallback=False),
+        ),
+        hostname=config.get('local', 'host', fallback='127.0.0.1'),
+        port=config.getint('local', 'port', fallback=25)
+    )
+    controller.start()
+    while controller.loop.is_running():
+        pass
