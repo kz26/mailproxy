46c471fb6d889ba337216c299df0c14577ee5682
Ahmon Dancy <dancy@dancysoft.com>
2019-10-09 14:24:47 -0700

    Avoid cascading error on SMTPNotSupportedError
    If delivery failed due to a SMTPNotSupportedError exception, the
    exception handler in _deliver would try to invoke the decode() method
    on the class object, resulting in a cascading error.  This commit
    fixes that problem.

--- mailproxy.py
+++ mailproxy.py
@@ -61,7 +61,7 @@ class MailProxyHandler:
             # All recipients were refused. If the exception had an associated
             # error code, use it.  Otherwise, fake it with a SMTP 554 status code. 
             errcode = getattr(e, 'smtp_code', 554)
-            errmsg = getattr(e, 'smtp_error', e.__class__)
+            errmsg = getattr(e, 'smtp_error', e.__class__.__name__.encode())
             raise smtplib.SMTPResponseException(errcode, errmsg.decode())
 
 
